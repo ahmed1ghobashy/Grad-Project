3092e38119f31ad92a1be40ba95d2dd7d6489bd5
ahmed1ghobashy <67714022+ahmed1ghobashy@users.noreply.github.com>
2023-06-24 17:04:20 +0300

    first commit

---
+++ Meal Prediction Api/app.py
@@ -0,0 +1,41 @@
+import io
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from flask import Flask, jsonify, request 
+import pickle
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+model = tf.keras.models.load_model('FinaalVersionOfModel.h5')
+classes = np.loadtxt('labels.txt',delimiter=',',dtype=str)
+
+def ImagePreprocessing(img):
+    img = Image.open(io.BytesIO(img))
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = np.expand_dims(img, 0)
+    return img
+
+def PredictMeal(img):
+    prediction = model.predict(img)
+    index = np.argmax(prediction)
+    meal_name = str(classes[index])
+    return meal_name
+
+@app.route('/predict', methods=['POST'])
+def PredictMealName():
+    if 'file' not in request.files:
+        return "Please try again. The Image doesn't exist"
+    file = request.files.get('file')
+    if not file:
+        return
+    meal_image = file.read()
+    img = ImagePreprocessing(meal_image)
+    return jsonify(MealName = PredictMeal(img))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
